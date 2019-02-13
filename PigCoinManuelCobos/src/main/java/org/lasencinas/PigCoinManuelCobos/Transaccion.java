0b9342c3cc23074566934bc5fe7da1498205010e
unknown <manuelcobosibanez@gmail.com>
2019-02-13 19:30:52 +0100

    creada clase Transaccion y sus atributos

---
+++ PigCoinManuelCobos/src/main/java/org/lasencinas/PigCoinManuelCobos/Transaccion.java
@@ -0,0 +1,15 @@
+package org.lasencinas.PigCoinManuelCobos;
+
+public class Transaccion {
+
+	
+	//Atributos
+	
+	private String hash = null;
+	private String prev_hash = null;
+	private PublicKey pKey_sender = null;
+	private PublicKey pKey_recipient = null;
+	private int pigcoins = 0;
+	private String message = null;
+	
+}
